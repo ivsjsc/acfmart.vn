77a49ccca58fe74d94698b00af2bd1e8ac775817
ivsjsc <ivscorp.vn@gmail.com>
2026-05-28 10:29:10 +0700

    commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,13 @@
 // Root build file for the acfmart monorepo
+// NOTE: Android projects should be built from their own directories
+// Do not run Gradle commands from root for Android builds
 
 plugins {
     // Add plugins here if needed
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
+// Only configure root project - do not apply to subprojects
+repositories {
+    google()
+    mavenCentral()
 }
